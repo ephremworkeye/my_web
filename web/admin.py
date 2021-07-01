11bfcf5d64df45e894ff2f6ee152cdd5fde179e8
Ephrem Wube <ephremworkeye@gmail.com>
2021-07-01 02:01:28 -0700

    admin registeration for Author and Post

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Author, Post
 
 # Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email','username')
+    
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'slug', 'description','username')
